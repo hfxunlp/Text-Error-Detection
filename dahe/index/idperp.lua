6a2e3f520882d891ef254420b68becb1d60738cd
ano <250081373@qq.com>
2016-10-03 11:46:31 +0800

    try SeqDropout
    add SeqDropout.

--- dahe/index/idperp.lua
+++ dahe/index/idperp.lua
@@ -2,6 +2,7 @@ require "nn"
 require "dpnn"
 require "rnn"
 require "SeqBLMGRU"
+require "SeqDropout"
 require "vecLookup"
 require "maskZerovecLookup"
 require "ASequencerCriterion"
@@ -15,7 +16,7 @@ function getnn()
 
 	local clsmod=nn.Sequencer(nn.MaskZero(nn.Sequential():add(nn.Linear(nfeature,nhidden)):add(nn.Tanh()):add(nn.Linear(nhidden,nclass)):add(nn.LogSoftMax()),1));
 
-	local nnmod=nn.Sequential():add(id2vec):add(coremod):add(nn.SplitTable(1)):add(clsmod);
+	local nnmod=nn.Sequential():add(id2vec):add(nn.SeqDropout()):add(coremod):add(nn.SplitTable(1)):add(clsmod);
 
 	return nnmod
 end
--- dahe/wvec/word/vecsim.lua
+++ dahe/wvec/word/vecsim.lua
@@ -2,6 +2,7 @@ require "nn"
 require "dpnn"
 require "rnn"
 require "SeqBLMGRU"
+require "SeqDropout"
 require "vecLookup"
 require "maskZerovecLookup"
 require "nngraph"
@@ -17,7 +18,7 @@ function getnn()
 	local clsmod=nn.Sequencer(nn.MaskZero(getgcnn(2,sizvec,2),1));
 
 	local nnmodi=nn.Sequential():add(coremod):add(nn.SplitTable(1)):add(clsmod);
-	local nnmod=nn.Sequential():add(id2vec):add(nn.ConcatTable():add(nnmodi):add(nn.SplitTable(1))):add(nn.ZipTable())
+	local nnmod=nn.Sequential():add(id2vec):add(nn.SeqDropout()):add(nn.ConcatTable():add(nnmodi):add(nn.SplitTable(1))):add(nn.ZipTable())
 
 	return nnmod
 end
